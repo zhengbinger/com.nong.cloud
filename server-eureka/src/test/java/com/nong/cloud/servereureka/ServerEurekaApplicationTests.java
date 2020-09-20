bf78cb58844e6320de135ce1fdb3a44b93fdea34
zhengbing <mydreambing@126.com>
2020-09-21 07:19:17 +0800

    add provider-payment and customer-order as eureka client

---
+++ server-eureka/src/test/java/com/nong/cloud/servereureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nong.cloud.servereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
