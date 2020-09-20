9e2b4da91d8823a27b6efc570116f2c2bbf5960c
zhengbing <mydreambing@126.com>
2020-09-21 07:18:56 +0800

    add provider-payment and customer-order as eureka client

--- provider-payment/src/main/java/com/nong/cloud/payment/ProviderPaymentApplication.java
+++ provider-payment/src/main/java/com/nong/cloud/payment/ProviderPaymentApplication.java
@@ -3,7 +3,10 @@ package com.nong.cloud.payment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/** @author zhengbing */
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.nong.cloud.payment.repository")
 public class ProviderPaymentApplication {
