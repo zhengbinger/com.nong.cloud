641e369627d93a2de9862cc5ef2111a566ce94eb
zhengbing <mydreambing@126.com>
2020-09-25 12:45:42 +0800

    finished  addition server-zookeeper module to registry-center

---
+++ server-zookeeper/README.md
@@ -0,0 +1,2 @@
+## Spring Cloud Zookeeper 作为服务注册中心
+Zookeeper 使用 临时节点作为服务节点
---
+++ server-zookeeper/src/test/java/com/nong/cloud/zookeeper/ServerZookeeperApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nong.cloud.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerZookeeperApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
