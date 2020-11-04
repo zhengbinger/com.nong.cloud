c17ebdb3a088aec56b9b38639dcbb80574d1814c
zhengbing <mydreambing@126.com>
2020-11-04 23:55:58 +0800

    addition spring cloud bus auto refresh  config file

--- cloud-client-server2/src/main/java/com/nonng/cloud/config/ConfigClientApplication.java
+++ cloud-client-server2/src/main/java/com/nonng/cloud/config/ConfigClientApplication.java
@@ -1,5 +1,6 @@
 package com.nonng.cloud.config;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -11,4 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ConfigClientApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientApplication.class, args);
+  }
 }
